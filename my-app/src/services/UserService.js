eccb8d3c71e42d429584ec5eda2fb985fc9d2188
Vinicius de Almeida <59939789+vinecx@users.noreply.github.com>
2021-08-17 16:59:47 -0300

    Update UserService.js
    Heroku Api em Node

--- my-app/src/services/UserService.js
+++ my-app/src/services/UserService.js
@@ -2,36 +2,33 @@
 export async function getAllUsers() {
 
     try{
-        const response = await fetch('/api/users');
+        const response = await fetch('https://vinecx-api.herokuapp.com/api/users');
         return await response.json();
     }catch(error) {
         return [];
     }
     
 }
-
 export async function deleteAllUsers() {
-    const response = await fetch('/api/users', {
+    const response = await fetch('https://vinecx-api.herokuapp.com/api/users', {
         method: 'delete',
         headers: {'Content-Type': 'application/json'},
     });
     return await response.json();
 }
-
 export async function deleteUsers(id) {
-    const response = await fetch(`/api/user/${id}`, {
+    const response = await fetch(`https://vinecx-api.herokuapp.com/api/user/${id}`, {
         method: 'delete',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({id: id})
     });
     return await response.json();
 }
-
 export async function createUser(data) {
-    const response = await fetch(`/api/user`, {
+    const response = await fetch(`https://vinecx-api.herokuapp.com/api/user`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({user: data})
       })
     return await response.json();
-}
+    }
